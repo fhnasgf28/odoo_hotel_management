ac5bccf1de3b2963f0e4449b39c1976ae652521f
fhnasgf <Farhan.Assegaf@hashmicro.com.sg>
2024-09-24 17:02:45 +0700

    [ADD] add cleaning_request_views.xml

--- models/room_booking.py
+++ models/room_booking.py
@@ -23,9 +23,9 @@ class RoomBooking(models.Model):
                                               help="sets to True if the maintenance request send")
     checkin_date = fields.Datetime(string="Check In", help="Date Of CheckIn", default=fields.Datetime.now())
     checkout_date = fields.Datetime(string="Check Out",
-                                    help="You can choose the date,"
-                                         " Otherwise sets to current Date",
-                                    required=True)
+                                    help="Date of Checkout",
+                                    default=fields.Datetime.now() + timedelta(
+                                        hours=23, minutes=59, seconds=59))
     hotel_policy = fields.Selection([
         ("prepaid", "On Booking"),
         ("manual", "On CheckIn"),
@@ -174,9 +174,129 @@ class RoomBooking(models.Model):
     def action_reserve(self):
         pass
 
-    def action_cancel(self):
-        pass
+    def _compute_invoice_count(self):
+        for record in self:
+            record.invoice_count = self.env['account.move'].search_count(
+                [('ref', '=', self.name)]
+            )
 
+    def action_cancel(self):
+        if self.room_line_ids:
+            for room in self.room_line_ids:
+                room.room_id.write({
+                    'status': 'available',
+                })
+
+    def _compute_amount_untaxed(self, flag=False):
+        """Compute the total amounts of the Sale Order"""
+        amount_untaxed_room = 0.0
+        amount_untaxed_food = 0.0
+        amount_untaxed_fleet = 0.0
+        amount_untaxed_event = 0.0
+        amount_untaxed_service = 0.0
+        amount_taxed_room = 0.0
+        amount_taxed_food = 0.0
+        amount_taxed_fleet = 0.0
+        amount_taxed_event = 0.0
+        amount_taxed_service = 0.0
+        amount_total_room = 0.0
+        amount_total_food = 0.0
+        amount_total_fleet = 0.0
+        amount_total_event = 0.0
+        amount_total_service = 0.0
+        room_lines = self.room_line_ids
+        food_lines = self.food_order_line_ids
+        service_lines = self.service_line_ids
+        fleet_lines = self.vehicle_line_ids
+        event_lines = self.event_line_ids
+        booking_list = []
+        account_move_line = self.env['account.move.line'].search_read(
+            domain=[('ref', '=', self.name),
+                    ('display_type', '!=', 'payment_term')],
+            fields=['name', 'quantity', 'price_unit', 'product_type'], )
+        for rec in account_move_line:
+            del rec['id']
+        if room_lines:
+            amount_untaxed_room += sum(room_lines.mapped('price_subtotal'))
+            amount_taxed_room += sum(room_lines.mapped('price_tax'))
+            amount_total_room += sum(room_lines.mapped('price_total'))
+            for room in room_lines:
+                booking_dict = {'name': room.room_id.name,
+                                'quantity': room.uom_qty,
+                                'price_unit': room.price_unit,
+                                'product_type': 'room'}
+                if booking_dict not in account_move_line:
+                    if not account_move_line:
+                        booking_list.append(booking_dict)
+                    else:
+                        for rec in account_move_line:
+                            if rec['product_type'] == 'room':
+                                if booking_dict['name'] == rec['name'] and \
+                                        booking_dict['price_unit'] == rec[
+                                    'price_unit'] and booking_dict['quantity'] \
+                                        != rec['quantity']:
+                                    booking_list.append(
+                                        {'name': room.room_id.name,
+                                         "quantity": booking_dict[
+                                                         'quantity'] - rec[
+                                                         'quantity'],
+                                         "price_unit": room.price_unit,
+                                         "product_type": 'room'})
+                                else:
+                                    booking_list.append(booking_dict)
+                    if flag:
+                        room.booking_line_visible = True
+        if food_lines:
+            for food in food_lines:
+                booking_list.append(self.create_list(food))
+            amount_untaxed_food += sum(food_lines.mapped('price_subtotal'))
+            amount_taxed_food += sum(food_lines.mapped('price_tax'))
+            amount_total_food += sum(food_lines.mapped('price_total'))
+        if service_lines:
+            for service in service_lines:
+                booking_list.append(self.create_list(service))
+            amount_untaxed_service += sum(
+                service_lines.mapped('price_subtotal'))
+            amount_taxed_service += sum(service_lines.mapped('price_tax'))
+            amount_total_service += sum(service_lines.mapped('price_total'))
+        if fleet_lines:
+            for fleet in fleet_lines:
+                booking_list.append(self.create_list(fleet))
+            amount_untaxed_fleet += sum(fleet_lines.mapped('price_subtotal'))
+            amount_taxed_fleet += sum(fleet_lines.mapped('price_tax'))
+            amount_total_fleet += sum(fleet_lines.mapped('price_total'))
+        if event_lines:
+            for event in event_lines:
+                booking_list.append(self.create_list(event))
+            amount_untaxed_event += sum(event_lines.mapped('price_subtotal'))
+            amount_taxed_event += sum(event_lines.mapped('price_tax'))
+            amount_total_event += sum(event_lines.mapped('price_total'))
+        for rec in self:
+            rec.amount_untaxed = amount_untaxed_food + amount_untaxed_room + \
+                                 amount_untaxed_fleet + \
+                                 amount_untaxed_event + amount_untaxed_service
+            rec.amount_untaxed_food = amount_untaxed_food
+            rec.amount_untaxed_room = amount_untaxed_room
+            rec.amount_untaxed_fleet = amount_untaxed_fleet
+            rec.amount_untaxed_event = amount_untaxed_event
+            rec.amount_untaxed_service = amount_untaxed_service
+            rec.amount_tax = (amount_taxed_food + amount_taxed_room
+                              + amount_taxed_fleet
+                              + amount_taxed_event + amount_taxed_service)
+            rec.amount_taxed_food = amount_taxed_food
+            rec.amount_taxed_room = amount_taxed_room
+            rec.amount_taxed_fleet = amount_taxed_fleet
+            rec.amount_taxed_event = amount_taxed_event
+            rec.amount_taxed_service = amount_taxed_service
+            rec.amount_total = (amount_total_food + amount_total_room
+                                + amount_total_fleet + amount_total_event
+                                + amount_total_service)
+            rec.amount_total_food = amount_total_food
+            rec.amount_total_room = amount_total_room
+            rec.amount_total_fleet = amount_total_fleet
+            rec.amount_total_event = amount_total_event
+            rec.amount_total_service = amount_total_service
+        return booking_list
     #     action check-in
     def action_checkin(self):
         pass
