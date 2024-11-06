52c6d8223789a2bd7a6206faf9c489c10ad84c3e
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-11-06 08:29:06 +0700

    [ADD] event_booking_line.py

--- models/event_booking_line.py
+++ models/event_booking_line.py
@@ -16,3 +16,36 @@ class EventBookingLine(models.Model):
     price_unit = fields.Float(related='ticket_id.lst_price', string='Price', digits='Product Price')
     tax_ids = fields.Many2many('account.tax', 'hotel_event_order_line_taxes_rel', 'event_id', 'tax_id', related='ticket_id.taxes_id',string='Taxes',
                                domain=[('type_tax_use', '=', 'sale')])
+    currency_id = fields.Many2one(string='Currency', related='booking_id.pricelist_id.currency_id', help="Total Price Currency", store=True, precompute=True)
+    price_subtotal = fields.Float(string='Subtotal', compute='_compute_price_subtotal', help='Total Price Excluding Tax', store=True)
+    price_tax = fields.Float(string="Total Tax", compute="_compute_price_subtotal", help="Tax amount", store=True)
+    price_total = fields.Float(string="Total", compute="_compute_price_subtotal", store=True)
+    state = fields.Selection(related='booking_id.state', string='Order Status', copy=False)
+
+    @api.depends('price_unit', 'uom_qty', 'tax_ids')
+    def _compute_price_subtotal(self):
+        for line in self:
+            tax_results = self.env['account.tax']._compute_taxes([line._convert_to_tax_base_line_dict()])
+            totals = list(tax_results['totals'].values())[0]
+            amount_untaxed = totals['amount_untaxed']
+            amount_tax = totals['amount_tax']
+            line.update({
+                'price_subtotal': amount_untaxed,
+                'price_tax': amount_tax,
+                'price_total': amount_untaxed + amount_tax,
+            })
+            if self.env.context.get('import_file', False) and not self.env.user. \
+                    user_has_groups('account.group_account_manager'):
+                line.tax_id.invalidate_recordset(['invoice_repartition_line_ids'])
+
+    def _convert_to_tax_base_line_dict(self):
+        self.ensure_one()
+        return self.env['account.tax']._convert_to_tax_base_line_dict(
+            self,
+            partner=self.booking_id.partner_id,
+            currency=self.currency_id,
+            taxes=self.tax_ids,
+            price_unit=self.price_unit,
+            quantity=self.uom_qty,
+            price_subtotal=self.price_subtotal,
+        )
